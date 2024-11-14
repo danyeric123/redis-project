4b51e23aee27e080c417e9a44bbf68cc88e15003
danyeric123 <dnagarpowers@gmail.com>
2024-11-14 17:59:52 -0500

    pong

--- app/main.py
+++ app/main.py
@@ -9,9 +9,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client, addr = server_socket.accept()  # wait for client
-    data = client.recv(1024)  # receive data from client
-    if data.strip() == b"PING":
-        iclient.send(b"+PONG\r\n")
+    client.send(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
