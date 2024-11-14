6fb2ccbf935cde29ad70105177bcd12abe5b7841
danyeric123 <dnagarpowers@gmail.com>
2024-11-14 18:13:23 -0500

    only on ping do pong

--- app/main.py
+++ app/main.py
@@ -8,9 +8,8 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    while True:
-        client, addr = server_socket.accept()  # wait for client
-        # data = client.recv(1024)  # receive data from client
+    client, addr = server_socket.accept()  # wait for client
+    while client.recv(1024):
         # # Decode the bytes to a string and split by the delimiter
         # data_string = data.decode('utf-8')
         # # The last part is what we need
