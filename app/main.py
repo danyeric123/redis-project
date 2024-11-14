c855a924e23925a4bb1f3eeda0e5832f06bcbd40
danyeric123 <dnagarpowers@gmail.com>
2024-11-14 18:12:27 -0500

    only on ping do pong

--- app/main.py
+++ app/main.py
@@ -8,13 +8,14 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client, addr = server_socket.accept()  # wait for client
-    data = client.recv(1024)  # receive data from client
-    # Decode the bytes to a string and split by the delimiter
-    data_string = data.decode('utf-8')
-    # The last part is what we need
-    extracted_value = data_string.split('\r\n')[-2].upper()
-    if extracted_value == "PING":
+    while True:
+        client, addr = server_socket.accept()  # wait for client
+        # data = client.recv(1024)  # receive data from client
+        # # Decode the bytes to a string and split by the delimiter
+        # data_string = data.decode('utf-8')
+        # # The last part is what we need
+        # extracted_value = data_string.split('\r\n')[-2].upper()
+        # if extracted_value == "PING":
         client.send(b"+PONG\r\n")
 
 if __name__ == "__main__":
